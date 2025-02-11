8f4a7edc2f3208b35e83e98f7697b4536a60cf88
xiuru.yang <xiuru.yang@tronfuture.com>
2025-02-11 11:19:42 +0800

    1. Creating Layouts and Pages 2. Navigating Between Pages

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'], weight: ['400'], });
+ 
+export const lusitana = Inter({ subsets: ['latin'], weight: ['400', '700'], });
